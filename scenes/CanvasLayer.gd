cec00c088d0d9ff69c3ef97b01786feb479ab9ca
Sarath <vssarathc04@gmail.com>
2021-03-03 19:50:01 +0000

    added coins == 0 no bullet can be shooted

--- scenes/CanvasLayer.gd
+++ scenes/CanvasLayer.gd
@@ -1,10 +1,18 @@
 extends CanvasLayer
-var coins = 0
+signal coins_zero
+var coins = 1
 
 func _ready():
 	$number_of_coins_collected.text = String(coins)
-	
 
 func _on_coin_coin_collected():
 	coins += 1
 	_ready()
+
+
+func _on_player_fired():
+	if coins != 0 :
+		coins -= 1
+		_ready()
+	else :
+		emit_signal("coins_zero")
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,13 +1,14 @@
 extends KinematicBody2D
-
+signal fired
 # velocity of player
 var velocity = Vector2(0,0)
-
+var coins_zero = false
 #setting the  linear velocity of the player
 export var v = 300
 const GRAVITY = 30
 export var JUMPFORCE = -700
 #funcion delta runs 60 times a second
+
 const FIREBALL = preload("res://scenes/attack.tscn")
 # warning-ignore:unused_argument
 func _physics_process(delta):
@@ -35,17 +36,20 @@ func _physics_process(delta):
 		
 		
 	if Input.is_action_just_pressed("fire"):
-		var fireball = FIREBALL.instance()
+		emit_signal("fired")
+		if coins_zero != true :
+			var fireball = FIREBALL.instance()
 	
-		if $AnimatedSprite.flip_h == true :
-			fireball.changingDirection(-1)
-		else :
-			fireball.changingDirection(1)
+			if $AnimatedSprite.flip_h == true :
+				fireball.changingDirection(-1)
+			else :
+				fireball.changingDirection(1)
 			
-		get_parent().add_child(fireball)
-		fireball.position = $Position2D.global_position
+			get_parent().add_child(fireball)
+			fireball.position = $Position2D.global_position
 	velocity = move_and_slide(velocity,Vector2.UP)
 	velocity.x = lerp(velocity.x , 0 , 0.2)
+
 
 
 # warning-ignore:unused_argument
@@ -60,3 +64,9 @@ func _on_Area2D_body_entered(body):
 func _on_death_range_body_entered(body):
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://scenes/leve1.tscn")
+
+
+
+
+func _on_CanvasLayer_coins_zero():
+	coins_zero = true
