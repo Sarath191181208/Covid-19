001b7d6873ec38fbb6c9a9fe3a5cbaf38f13f10f
Sarath <vssarathc04@gmail.com>
2021-03-03 05:42:38 +0000

    1)Added tiles. 2)Added mobile controls . 3)fixed bugs . 4) found player bug : player collision layer translates when he flips imp

---
+++ buttons.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+func _on_up_pressed():
+	Input.action_press("jump")
+	
+func _on_up_released():
+	Input.action_release("jump")
+
+
+func _on_left_pressed():
+	Input.action_press("left")
+
+
+func _on_left_released():
+	Input.action_release("left")
+
+
+func _on_right_pressed():
+	Input.action_press("right")
+
+
+func _on_right_released():
+	Input.action_release("right")
